4df923f86d0663baf861c128d57edc322d8e5c0c
Wenwen Tian <wwtian9@gmail.com>
2021-12-26 16:40:43 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,5 @@
-// var playerName = 'Clank McKrank';
+// 3.1 
+//var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
